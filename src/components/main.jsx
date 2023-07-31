b9e348aa6b48504a844b58b118aecb4800420109
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2023-07-31 17:07:13 +0100

    Add files via upload

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,42 @@
+import famsi from "../assets/Famasi-3.png"
+import footcity from "../assets/foot-city.png"
+
 
 function Main() {
     return(
-        <main className="dark">
+        <main className="h-screen">
+        <div>
+            <h1 className=" text-8xl font-extrabold p-10 flex flex-col">
+                    <span className="text-orange-400 p-2">Endless<br/></span> 
+                    <span className="text-orange-400 p-2">possibilities<br/></span>
+                    <span className="p-2">for all</span>
+            </h1>
+            <div className="pl-10 text-2xl w-1/2">
+            <p>
+                Unlocking boundless payment opportunities for enterprises, 
+                individuals, small businesses, emerging markets, and startups alike.
+            </p>
+        </div>
+        <div className="p-10">
+            <button className="bg-black text-white mr-10 p-4 rounded-md">Crerate a free account</button>
+            <button className="bg-slate-200 mr-10 p-4 rounded-md dark:text-blue-950">Contact sales</button>
+        </div>
+        </div>
+        <div className="flex gap-14">
+            <div className="items-center">
+                <img src={footcity} alt="footcity" className="relative top-52 w-98 rounded-xl"/>
+            </div>
+            <div>
+                <img src={famsi} alt="famsi" className="relative bottom-15 w-3/5 rounded-xl"/>
+                <p className="text-2xl p-2">
+                Send money home to loved ones, sell online as a small business, 
+                process global payments as an enterprise,
+                 build financial products as a startup. With Flutterwave, 
+                the question is not what is possible—it is: what is not?
+                </p>
+            </div>
+        </div>
+
 
         </main>
     )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,13 +7,6 @@ import flutterwavelogo from '../assets/Flutterwave-Logo.png';
 
     const [darkToggle, setDarkToggle] = useState(null)
 
-//    useEffect(() => {
-//        if (window.matchMedia('(prefers-color-scheme: dark)').matches){
-//            setDarkToggle('dark')
-//        }else{
-//            setDarkToggle('light')
-//        }
-//    }, [])
 
     useEffect(() => {
         if ( darkToggle === "dark") {
@@ -35,23 +28,23 @@ import flutterwavelogo from '../assets/Flutterwave-Logo.png';
         <nav className='p-10 flex items-center' >
         <img src= {flutterwavelogo} alt='flutterwavelogo' className=' w-40 flex' />
         
-        <ul className='hidden lg:flex items-center pl-64 m'>
+        <ul className='hidden lg:flex items-center pl-64 font-semibold '>
         
-            <li className='mr-5'>Enterprise</li>
-            <li className='mr-5'>SME</li>
-            <li className='mr-5'>Personal</li>
-            <li className='mr-5'>Resources</li>
-            <li className='mr-5' >Developers</li>
-            <li className='mr-5'>Company</li>
+            <li className=' mr-5 hover:underline decoration-orange-400 cursor-pointer'>Enterprise</li>
+            <li className='mr-5 hover:underline decoration-orange-400 cursor-pointer'>SME</li>
+            <li className='mr-5 hover:underline decoration-orange-400 cursor-pointer'>Personal</li>
+            <li className='mr-5 hover:underline decoration-orange-400 cursor-pointer'>Resources</li>
+            <li className='mr-5 hover:underline decoration-orange-400 cursor-pointer' >Developers</li>
+            <li className='mr-5 hover:underline decoration-orange-400 cursor-pointer'>Company</li>
 
         </ul>
-        <ul className='lg:flex items-center ml-auto'>
-            <li className='hidden lg:mr-16'><button>Sign in</button></li>
+        <ul className='hidden lg:flex items-center ml-auto'>
+            <li className='lg:mr-16'><button className='bg-slate-200 p-2 pl-10 pr-10 rounded-md hover:bg-blue-950 hover:text-white dark:text-blue-950 hover:dark:text-white'>Sign in</button></li>
             <li>
                 <label className="lg:relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" className="hidden lg:sr-only peer"/>
-                    <div onClick={ToggleDarkMode} className="hidden lg:w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-300"></div>
-                    <span className="hidden lg:ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark mode</span>
+                    <input type="checkbox" value="" className="lg:sr-only peer"/>
+                    <div onClick={ToggleDarkMode} className="lg:w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-300"></div>
+                    <span className="lg:ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark mode</span>
                 </label>
             </li>
             </ul>
